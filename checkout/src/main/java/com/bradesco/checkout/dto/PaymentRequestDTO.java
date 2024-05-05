887cc1d51c8569e3f2eacf0339404727330e65c6
Bruno Queiroz <bruno.queiroz@liferay.com>
2024-05-05 16:11:45 -0300

    BRD-0001 | Create request and reponse DTO's.

--- checkout/src/main/java/com/bradesco/checkout/dto/PaymentRequestDTO.java
+++ checkout/src/main/java/com/bradesco/checkout/dto/PaymentRequestDTO.java
@@ -1,2 +1,41 @@
-package com.bradesco.checkout.dto;public class PaymentRequestDTO {
+package com.bradesco.checkout.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentRequestDTO implements Serializable {
+    private static final long serialVersionUID = -8966467086517094635L;
+
+    @JsonProperty("value")
+    @NotEmpty
+    private BigDecimal value;
+
+    @JsonProperty("date_of_transaction")
+    @NotEmpty
+    private String transaction_date;
+
+    @NotEmpty
+    @JsonProperty("pix-key")
+    private String pixKey;
+
+    @JsonProperty("bankName")
+    @NotEmpty
+    private String bankName;
+
+    @JsonProperty("bankCode")
+    @NotEmpty
+    private long bankCode;
+
 }
--- checkout/src/main/java/com/bradesco/checkout/dto/PaymentResponseDTO.java
+++ checkout/src/main/java/com/bradesco/checkout/dto/PaymentResponseDTO.java
@@ -1,2 +1,28 @@
-package com.bradesco.checkout.dto;public class PaymentResponseDTO {
+package com.bradesco.checkout.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentResponseDTO implements Serializable {
+    private static final long serialVersionUID = -6530322009713064068L;
+
+    @JsonProperty("status_transaction")
+    private String status_transaction;
+
+    @JsonProperty("date")
+    private String date_time_transaction;
+
+    @JsonProperty("qrCode-image")
+    private String qrCode;
 }
