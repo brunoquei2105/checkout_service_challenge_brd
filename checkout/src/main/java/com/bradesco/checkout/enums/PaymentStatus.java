7606f7085e4717564718688a355caeb2a596c551
Bruno Queiroz <bruno.queiroz@liferay.com>
2024-05-05 16:14:36 -0300

    BRD-0001 | Create PaymentStatus enum.

--- checkout/src/main/java/com/bradesco/checkout/enums/PaymentStatus.java
+++ checkout/src/main/java/com/bradesco/checkout/enums/PaymentStatus.java
@@ -1,2 +1,12 @@
-package com.bradesco.checkout;public enum PaymentStatus {
+package com.bradesco.checkout.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentStatus {
+
+    PENDING,
+    APPROVED,
+    IN_PROGRESS,
+    NOT_APPROVED;
 }
