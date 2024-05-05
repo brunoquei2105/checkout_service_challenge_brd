18339b0bdd5fb3d4d68aac98463e2dea181c2903
Bruno Queiroz <bruno.queiroz@liferay.com>
2024-05-05 16:12:41 -0300

    BRD-0001 | Create JPA entity-Payment.

--- checkout/src/main/java/com/bradesco/checkout/model/Payment.java
+++ checkout/src/main/java/com/bradesco/checkout/model/Payment.java
@@ -1,2 +1,44 @@
-package com.bradesco.checkout.model;public class Payment {
+package com.bradesco.checkout.model;
+
+import com.bradesco.checkout.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "payment_tb")
+public class Payment implements Serializable {
+    private static final long serialVersionUID = -4412948369635862300L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private BigDecimal value;
+
+    private long orderId;
+
+    private LocalTime transaction_date;
+
+    private PaymentStatus status;
+
+    private String pixKey;
+
+    private String bankName;
+
+    private String bankCode;
+
+
+
+
 }
