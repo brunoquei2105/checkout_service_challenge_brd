2caa9ac4b2e6134658bc81e185016d89666f8c11
Bruno Queiroz <bruno.queiroz@liferay.com>
2024-05-05 16:10:50 -0300

    BRD-0001 | Create repository for Payment model.

--- checkout/src/main/java/com/bradesco/checkout/repository/PaymentRepository.java
+++ checkout/src/main/java/com/bradesco/checkout/repository/PaymentRepository.java
@@ -1,2 +1,10 @@
-package com.bradesco.checkout.repository;public class PaymentRepository {
+package com.bradesco.checkout.repository;
+
+import com.bradesco.checkout.model.Payment;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends CrudRepository<Long, Payment> {
+
 }
